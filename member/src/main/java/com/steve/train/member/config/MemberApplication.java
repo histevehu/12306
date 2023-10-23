064d3416f66c69229e8d90ab45963b4a8369d110
SteveHu <histevehu@outlook.com>
2023-10-23 11:05:52 +0800

    修改启动日志，增加/member前缀，修改logback日志

--- member/src/main/java/com/steve/train/member/config/MemberApplication.java
+++ member/src/main/java/com/steve/train/member/config/MemberApplication.java
@@ -1,8 +1,11 @@
 package com.steve.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 //Springboot默认扫描Application所在包及其子包内的组件。
@@ -10,8 +13,13 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.steve")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Member Application，启动！");
+        LOG.info("测试地址：http://localhost:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 
 }
