c715c9ba9f51fc0826e93c3997999ee0423db8f4
SteveHu <histevehu@outlook.com>
2023-10-23 09:25:14 +0800

    初始化

---
+++ src/test/java/com/stevehu/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevehu.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
