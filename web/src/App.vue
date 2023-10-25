cf5f2276251a97f702e045ea16365b3c26bf73f0
SteveHu <histevehu@outlook.com>
2023-10-25 14:53:44 +0800

    登录视图前端部分完成

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,31 +1,9 @@
 <!--App.vue即每个页面都有的公共部分-->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <!--  <nav>-->
+  <!--    <router-link to="/">Home</router-link> |-->
+  <!--    <router-link to="/about">About</router-link>-->
+  <!--  </nav>-->
+  <!--router-view由router替换为对应的vue页面-->
   <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
