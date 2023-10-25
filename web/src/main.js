2e954b420f9fb93fff9a201f160097346d27d6ca
SteveHu <histevehu@outlook.com>
2023-10-25 14:12:09 +0800

    引入Ant Design及图标组件

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,19 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入 Ant Design
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue'
 
 // 启动App，启用store、router组件，并将内容渲染到public/index.html中名为app的div内
 // 通过 main.js（框架配置入口），将 index.html(html页面入口）和 App.vue（vue页面入口）关联起来
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app')
+
+// 注册组件
+const icons = Icons
+for (const i in icons) {
+    app.component(i, icons[i])
+}
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>Ant Design</a-button>
+    <star-outlined/>
+    <star-filled/>
+    <star-two-tone two-tone-color="#eb2f96"/>
   </div>
 </template>
