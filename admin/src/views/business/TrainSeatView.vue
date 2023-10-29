e2ac3eb4a77434ffeb11332b725884452ecadfbb
SteveHu <histevehu@outlook.com>
2023-10-29 17:16:44 +0800

    admin前端火车座位页面车次使用封装组件

--- admin/src/views/business/TrainSeatView.vue
+++ admin/src/views/business/TrainSeatView.vue
@@ -42,7 +42,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainSeat" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-input v-model:value="trainSeat.trainCode"/>
+        <TrainSelectView v-model="trainSeat.trainCode"></TrainSelectView>
       </a-form-item>
       <a-form-item label="厢序">
         <a-input v-model:value="trainSeat.carriageIndex"/>
@@ -77,9 +77,11 @@
 import {defineComponent, onMounted, ref} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/TrainSelect.vue";
 
 export default defineComponent({
   name: "TrainSeatView",
+  components: {TrainSelectView},
   setup() {
     const SEAT_COL_ARRAY = window.SEAT_COL_ARRAY;
     const SEAT_TYPE_ARRAY = window.SEAT_TYPE_ARRAY;
