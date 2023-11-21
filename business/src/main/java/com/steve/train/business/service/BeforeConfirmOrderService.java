f6ee7368a53f51d5230cbd7bbb9e15a3fe243612
SteveHu <histevehu@outlook.com>
2023-11-22 00:15:06 +0800

    买票功能增加体验排队购票

--- business/src/main/java/com/steve/train/business/service/BeforeConfirmOrderService.java
+++ business/src/main/java/com/steve/train/business/service/BeforeConfirmOrderService.java
@@ -46,53 +46,59 @@ public class BeforeConfirmOrderService {
 
     /**
      * 业务前逻辑。先进行令牌校验，若通过则将订单发送MQ以完成购票的后半程业务流程完成购票
+     *
      * @return 订单ID
      */
     @SentinelResource(value = "beforeDoConfirm", blockHandler = "beforeDoConfirmBlock")
     public long beforeDoConfirm(ConfirmOrderDoReq req) throws InterruptedException {
-        // 根据线程上下文获取用户ID插入到请求中
-        req.setMemberId(MemberLoginContext.getId());
-        // 校验并尝试获取令牌
-        String dlSkTokenKey = skTokenService.validSkToken(req.getDate(), req.getTrainCode(), req.getMemberId());
-        if (StrUtil.isNotEmpty(dlSkTokenKey)) {
-            LOG.info("令牌校验通过");
-        } else {
-            LOG.info("令牌校验不通过");
-            throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_SK_TOKEN_FAIL);
+        long id = -1;
+        // 根据前端传值，加入排队人数以体验排队购票场景
+        for (int i = 0; i < req.getLineNumber() + 1; i++) {
+            // 根据线程上下文获取用户ID插入到请求中
+            req.setMemberId(MemberLoginContext.getId());
+            // 校验并尝试获取令牌
+            String dlSkTokenKey = skTokenService.validSkToken(req.getDate(), req.getTrainCode(), req.getMemberId());
+            if (StrUtil.isNotEmpty(dlSkTokenKey)) {
+                LOG.info("令牌校验通过");
+            } else {
+                LOG.info("令牌校验不通过");
+                throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_SK_TOKEN_FAIL);
+            }
+            Date date = req.getDate();
+            String trainCode = req.getTrainCode();
+            String start = req.getStart();
+            String end = req.getEnd();
+            DateTime now = DateTime.now();
+            List<ConfirmOrderTicketReq> tickets = req.getTickets();
+            // 初始状态订单保存到确认订单表
+            ConfirmOrder confirmOrder = new ConfirmOrder();
+            confirmOrder.setId(SnowFlakeUtil.getSnowFlakeNextId());
+            confirmOrder.setCreateTime(now);
+            confirmOrder.setUpdateTime(now);
+            confirmOrder.setMemberId(req.getMemberId());
+            confirmOrder.setDate(date);
+            confirmOrder.setTrainCode(trainCode);
+            confirmOrder.setStart(start);
+            confirmOrder.setEnd(end);
+            confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+            confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
+            confirmOrder.setTickets(JSON.toJSONString(tickets));
+            confirmOrderMapper.insert(confirmOrder);
+            // 发送MQ排队购票
+            // MQ内消息代表指定日期车次的订单来了，等待处理。待分配到消费者后，对该消息指定的日期车次的订单循环逐张出票，直到所有订单处理完毕
+            ConfirmOrderMQDTO confirmOrderMQDTO = new ConfirmOrderMQDTO();
+            confirmOrderMQDTO.setDate(req.getDate());
+            confirmOrderMQDTO.setTrainCode(req.getTrainCode());
+            // 由于使用MQ异步处理启用了新的线程，所以原服务中的事件流水号不会自动传递过来，需要通过req对象传递
+            // 同理，若使用Spring自带的@Async，也需要手动传递事件流水号
+            confirmOrderMQDTO.setLogId(MDC.get("LOG_ID"));
+            String reqJson = JSON.toJSONString(confirmOrderMQDTO);
+            LOG.info("购票请求，发送mq开始，消息：{}", reqJson);
+            rocketMQTemplate.convertAndSend(RocketMQTopicEnum.CONFIRM_ORDER.getCode(), reqJson);
+            LOG.info("购票请求，发送mq结束");
+            id = confirmOrder.getId();
         }
-        Date date = req.getDate();
-        String trainCode = req.getTrainCode();
-        String start = req.getStart();
-        String end = req.getEnd();
-        DateTime now = DateTime.now();
-        List<ConfirmOrderTicketReq> tickets = req.getTickets();
-        // 初始状态订单保存到确认订单表
-        ConfirmOrder confirmOrder = new ConfirmOrder();
-        confirmOrder.setId(SnowFlakeUtil.getSnowFlakeNextId());
-        confirmOrder.setCreateTime(now);
-        confirmOrder.setUpdateTime(now);
-        confirmOrder.setMemberId(req.getMemberId());
-        confirmOrder.setDate(date);
-        confirmOrder.setTrainCode(trainCode);
-        confirmOrder.setStart(start);
-        confirmOrder.setEnd(end);
-        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
-        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
-        confirmOrder.setTickets(JSON.toJSONString(tickets));
-        confirmOrderMapper.insert(confirmOrder);
-        // 发送MQ排队购票
-        // MQ内消息代表指定日期车次的订单来了，等待处理。待分配到消费者后，对该消息指定的日期车次的订单循环逐张出票，直到所有订单处理完毕
-        ConfirmOrderMQDTO confirmOrderMQDTO = new ConfirmOrderMQDTO();
-        confirmOrderMQDTO.setDate(req.getDate());
-        confirmOrderMQDTO.setTrainCode(req.getTrainCode());
-        // 由于使用MQ异步处理启用了新的线程，所以原服务中的事件流水号不会自动传递过来，需要通过req对象传递
-        // 同理，若使用Spring自带的@Async，也需要手动传递事件流水号
-        confirmOrderMQDTO.setLogId(MDC.get("LOG_ID"));
-        String reqJson = JSON.toJSONString(confirmOrderMQDTO);
-        LOG.info("购票请求，发送mq开始，消息：{}", reqJson);
-        rocketMQTemplate.convertAndSend(RocketMQTopicEnum.CONFIRM_ORDER.getCode(), reqJson);
-        LOG.info("购票请求，发送mq结束");
-        return confirmOrder.getId();
+        return id;
     }
 
     public void beforeDoConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
